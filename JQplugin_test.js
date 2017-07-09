d009dd736a74228b5d6c045b34a43fba7d4b7f78
U10316043 <linannie841102@gmail.com>
2017-07-10 00:54:44 +0800

    練習更改顏色和標題 #1

---
+++ JQplugin_test.js
@@ -0,0 +1,32 @@
+(function($) {
+	
+	var defaultColor = {
+		color : "pink"
+	};
+	$.fn.fill = function(options) {
+		if(options){
+			var settings = $.extend({color: defaultColor}, options);
+			return this.css({color: settings.color});
+		}else{
+			return this.css({color: defaultColor.color});
+		}
+	};
+	$.fill = function(options) {
+		if(options){
+			var settings = $.extend({color: defaultColor}, options);
+			$("body").css({"background-color": settings.color});
+		}else{
+			$("body").css({"background-color": defaultColor.color});
+		}
+	}
+	
+	$.fill.options = defaultColor;
+	$.title = function(options){
+		if(options){
+			$(this).attr("title", options);
+		}else{
+			var getTitle = $(this).attr("title");
+		}
+	}
+	
+}(jQuery));
